7aa73560de644c838006b66509d63542d213a82c
dotSality <sality33@gmail.com>
2023-02-28 23:31:13 +0100

    -> warning message when press enter key

--- src/app/LoadingMessage.js
+++ src/app/LoadingMessage.js
@@ -11,14 +11,35 @@ export class LoadingMessage extends EventEmitter {
     this._loadingBar = this._app.loadingBar;
     this._resources = this._app.resources;
 
+    this._triggerEnterMessageCallback = (e) => {
+      if (e.key === "Enter") {
+        if (this._timeoutId) {
+          return;
+        }
+        const enterMessageElement = document.querySelector(".enter-message");
+        enterMessageElement.classList.add("appear");
+        const enterMessageProgressBar = document.createElement("div");
+        enterMessageProgressBar.className = "enter-message_progress-bar";
+        enterMessageElement.appendChild(enterMessageProgressBar);
+        this._timeoutId = setTimeout(() => {
+          enterMessageElement.classList.remove("appear");
+          enterMessageProgressBar.remove();
+          this._timeoutId = null;
+        }, 3000);
+      }
+    };
+
     this._app.resources.on(EVENTS_ENUM.READY, () => {
       this._messageElement.classList.add("fade-in");
       const clickableElement = document.querySelector(".hello-message_clickable");
       clickableElement.addEventListener("click", () => {
         this._messageElement.classList.add("fade-out");
         this._messageElement.style.pointerEvents = "none";
+        window.removeEventListener("keydown", this._triggerEnterMessageCallback);
         this.trigger(EVENTS_ENUM.READY_CLICK);
       });
+
+      window.addEventListener("keydown", this._triggerEnterMessageCallback);
     });
 
     this._loadingBar.on(EVENTS_ENUM.FADING_FINISHED, () => {
