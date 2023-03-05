d457125cf3e88bb9e7b519d25aff7c69b97da2b4
dotSality <sality33@gmail.com>
2023-03-05 22:44:03 +0100

    -> mobile placeholder, html edits

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import "./style.css";
-// import testVertexShader from "./shaders/broken-tv/brokenTvVertex.glsl";
-// import testFragmentShader from "./shaders/broken-tv/brokenTvFragment.glsl";
 import { App } from "./app/App";
+import { mobileDetectionRegex } from "./constants/regex";
 
-const canvas = document.querySelector(".webgl");
-const app = new App(canvas);
+export const isMobile = mobileDetectionRegex.test(navigator.userAgent);
+console.log(isMobile);
+
+if (isMobile) {
+  const div = document.createElement("div");
+  div.className = "mobile-placeholder";
+  div.innerText = "Unfortunately, mobile devices can't use our app in the way we want since some features wont work proper on some mobile devices, " +
+    "so we highly recommend you to enter our website from your PC!";
+  document.body.appendChild(div);
+} else {
+  document.body.innerHTML = `<canvas class="webgl"></canvas>
+    <div class="hello-message">PRESS
+      <span class="hello-message_clickable">
+        ENTER
+      </span>
+    </div>
+    <div class="enter-message">
+      Wrong :)
+    </div>`;
+  const canvas = document.querySelector(".webgl");
+  const app = new App(canvas);
+}
